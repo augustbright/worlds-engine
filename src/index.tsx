bd335c1537724633ce82b030a08192e94e1e440a
vtumanov782 <vtumanov782@gmail.com>
2021-08-03 22:05:12 +0300

    fonts

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,7 @@ import ReactDOM from "react-dom";
 import App from "components/app";
 import { initSW } from "./service-workers";
 
+require("./style.css");
+
 initSW();
 ReactDOM.render(<App />, document.getElementById("react-root"));
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,14 @@ module.exports = {
         exclude: /node_modules/,
         use: ['ts-loader'],
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },      
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
