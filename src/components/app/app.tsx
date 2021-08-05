0751e7905fb2b54fc983c4bc78f1caaa70fe37e8
vtumanov782 <vtumanov782@gmail.com>
2021-08-05 22:35:44 +0300

    formatted app.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,23 +1,29 @@
 import React, { useCallback, useState } from "react";
 import { ThemeProvider } from "styled-components";
 import { code } from "components/theming";
-import { id, entities, info, filtrator } from "../../mocks/descriptors";
 import DescriptorEditor from "components/blocks/descriptor-editor";
 import { PureTypeDescriptor } from "types/descriptors";
+import { id, entities, info, filtrator } from "../../mocks/descriptors";
 
 const mockTypeDescriptors = [id, entities, info, filtrator];
 
 export default (): JSX.Element => {
   const [typeDescriptors, setTypeDescriptors] = useState(mockTypeDescriptors);
-  const onChangeTypeDescriptors = useCallback((newTypes: Array<PureTypeDescriptor>) => {
-    setTypeDescriptors(newTypes);
-  }, [setTypeDescriptors]);
+  const onChangeTypeDescriptors = useCallback(
+    (newTypes: Array<PureTypeDescriptor>) => {
+      setTypeDescriptors(newTypes);
+    },
+    [setTypeDescriptors]
+  );
 
   return (
     <div>
       <ThemeProvider theme={code}>
-          <DescriptorEditor types={typeDescriptors} onChangeTypes={onChangeTypeDescriptors}/>
+        <DescriptorEditor
+          types={typeDescriptors}
+          onChangeTypes={onChangeTypeDescriptors}
+        />
       </ThemeProvider>
     </div>
-  )
+  );
 };
