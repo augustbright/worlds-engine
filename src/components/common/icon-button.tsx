859914d7feb90375ccaa2d102e681313faa41d90
vtumanov782 <vtumanov782@gmail.com>
2021-09-22 21:39:51 +0300

    auth page styling

--- src/components/common/icon-button.tsx
+++ src/components/common/icon-button.tsx
@@ -3,15 +3,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 import { fromThemeProp } from "components/theming/utils";
 import { Color } from "components/theming";
+import { Space } from "components/theming/types";
 
-const ButtonContainer = styled.div`
+const ButtonContainer = styled.a`
   display: inline-flex;
   align-items: center;
   justify-content: center;
   cursor: pointer;
   color: ${fromThemeProp((t) => t.colors[Color.LIST_PATH_FOREGROUND])};
-  width: 22px;
+  min-width: 22px;
   height: 22px;
+  text-decoration: none;
 
   &:hover {
     color: ${fromThemeProp((t) => t.colors[Color.BUTTON_HOVER_FOREGROUND])};
@@ -21,22 +23,39 @@ const ButtonContainer = styled.div`
   }
 `;
 
+const TextContainer = styled.span`
+  margin-left: ${fromThemeProp((t) => t.space[Space.MEDIUM])};
+`;
+
 type Props = {
-  onClick: () => void;
+  onClick?: () => void;
 };
 
 export const IconButton = forwardRef<
-  HTMLDivElement,
-  Omit<React.ComponentProps<typeof FontAwesomeIcon>, "onClick"> & Props
->(({ className, onClick, ...rest }, ref) => {
-  const handleClick = useCallback(() => {
-    onClick();
-  }, [onClick]);
+  HTMLAnchorElement,
+  Omit<React.ComponentProps<typeof FontAwesomeIcon>, "onClick"> &
+    React.PropsWithChildren<Props>
+>(({ className, onClick, children, href, ...rest }, ref) => {
+  const handleClick = useCallback(
+    (event: React.MouseEvent<HTMLAnchorElement>) => {
+      if (onClick) {
+        event.preventDefault();
+        onClick();
+      }
+    },
+    [onClick]
+  );
 
   return (
     <>
-      <ButtonContainer ref={ref} className={className} onClick={handleClick}>
+      <ButtonContainer
+        href={href}
+        ref={ref}
+        className={className}
+        onClick={handleClick}
+      >
         <FontAwesomeIcon {...rest} />
+        {children ? <TextContainer>{children}</TextContainer> : null}
       </ButtonContainer>
     </>
   );
--- src/pages/auth-page.tsx
+++ src/pages/auth-page.tsx
@@ -1,9 +1,60 @@
 import { getApi } from "api/utils";
+import { IconButton } from "components/common/icon-button";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { Space } from "components/theming/types";
+import { fromThemeProp } from "components/theming/utils";
 import React from "react";
+import styled from "styled-components";
 import { LayoutBase } from "./layout/base";
+
+const Center = styled.div`
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Header = styled.h1`
+  text-transform: uppercase;
+  border-bottom: solid 1px rgb(28, 25, 32);
+  width: 300px;
+  display: flex;
+  justify-content: center;
+  margin-bottom: ${fromThemeProp((t) => t.space[Space.HUGE])};
+  padding-bottom: ${fromThemeProp((t) => t.space[Space.LARGE])};
+  text-shadow: 0 0 10px rgb(224 206 237 / 28%);
+  color: #ece1f4;
+  padding-bottom: 8px;
+`;
+
+const AuthContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: ${fromThemeProp((t) => t.space[Space.HUGE])}
+    ${fromThemeProp((t) => t.space[Space.LARGE])};
+  border: solid 1px rgb(28, 25, 32);
+  background-color: rgb(21, 18, 23);
+`;
+
+const StyledIconButton = styled(IconButton)`
+  text-decoration: underline;
+`;
 
 export const AuthPage: React.FC = () => (
   <LayoutBase>
-    <a href={`${getApi()}/auth/google`}>Log in with google</a>
+    <Center>
+      <AuthContainer>
+        <Header>Worlds</Header>{" "}
+        <StyledIconButton
+          href={`${getApi()}/auth/google`}
+          icon={faGoogle}
+          size="sm"
+        >
+          Log in with google
+        </StyledIconButton>
+      </AuthContainer>
+    </Center>
   </LayoutBase>
 );
